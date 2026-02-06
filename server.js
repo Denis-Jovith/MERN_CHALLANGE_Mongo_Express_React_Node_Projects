921443f926713a87b7e4c070ca5979908eb17ea2
Denis Jovith <denisjovitusbuberwa@gmail.com>
2026-02-06 09:13:11 +0300

    server and path and listen ports

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3500
+const path = require('path')
 
+app.use('/',express.static(path.join(__dirname, '/public')))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
