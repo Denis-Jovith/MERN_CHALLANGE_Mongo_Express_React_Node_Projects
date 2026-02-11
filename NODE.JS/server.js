5fdf2657a03b61325368a1fa0b9bdb225d74077b
Denis Jovith <denisjovitusbuberwa@gmail.com>
2026-02-11 17:54:07 +0300

    imported and used modules from other files

--- NODE.JS/server.js
+++ NODE.JS/server.js
@@ -1,19 +1,29 @@
-console.log("Hello World")
+// console.log("Hello World")
 
-console.log(global);
+// console.log(global);
 
 const os = require('os')
 const path = require('path')
+const math=require('./math')
+const {add,substract,multiply,divide} = require('./math')
 
-console.log(os.type())
-console.log(os.version())
-console.log(os.homedir())
 
-console.log(__dirname)
-console.log(__filename)
+console.log(math.add(2,3))
+console.log(math.substract(4,7))
+console.log(add(2.9,43))
+console.log(substract(6,8))
+console.log(multiply(33,900))
+console.log(divide(-994,988))
 
-console.log(path.dirname(__filename))
-console.log(path.basename(__filename))
-console.log(path.extname(__filename))
+// console.log(os.type())
+// console.log(os.version())
+// console.log(os.homedir())
 
-console.log(path.parse(__filename))
+// console.log(__dirname)
+// console.log(__filename)
+
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
+
+// console.log(path.parse(__filename))
