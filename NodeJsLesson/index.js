39a3298227236c7bb94daf517336ce73e416502a
Denis Jovith <denisjovitusbuberwa@gmail.com>
2026-02-12 08:42:53 +0300

    read file, write file.append file

--- NodeJsLesson/index.js
+++ NodeJsLesson/index.js
@@ -13,6 +13,21 @@ fs.readFile(path.join(__dirname,'starter.txt'),'utf8',(err,data) => {
 )
 console.log('Hello..........')
 
+fs.writeFile(path.join(__dirname,'reply.txt'),'Nice to meet you',(err) => {
+    if(err) throw err;
+    console.log('Write Complete')
+})
+
+fs.readFile(path.join(__dirname,'reply.txt'),'utf8',(err,data) => {
+    if (err) throw err;
+    console.log(`The reply.txt is:   ${data}`)
+})
+
+fs.appendFile(path.join(__dirname,'testing.txt'),'Testing text',(err) => {
+    if (err) throw err;
+    console.log('Append complete')
+})
+
 process.on('uncaughtException', err => {
     console.error(`There was an uncaught error: ${err}`);
     process.exit(1);
