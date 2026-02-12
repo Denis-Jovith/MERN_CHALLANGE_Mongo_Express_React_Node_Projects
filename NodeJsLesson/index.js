9ecd5a0af652b4a398645879baf336aabd3a16ce
Denis Jovith <denisjovitusbuberwa@gmail.com>
2026-02-12 09:04:10 +0300

    sync and wait functions in reading

--- NodeJsLesson/index.js
+++ NodeJsLesson/index.js
@@ -1,34 +1,47 @@
-const fs = require('fs');
+// const fs = require('fs');
 const path= require('path');
+const fsPromises = require('fs').promises;
+
+const fileOps = async () => {
+    try{
+        const data = await fsPromises.readFile(path.join(__dirname,'starter.txt'),'utf8');
+        console.log(data);
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+fileOps();
 
 // fs.readFile('./starter.txt','utf8',(err,data) => {
 //     if (err) throw err;
 //     console.log(data);
 // })
 
-fs.readFile(path.join(__dirname,'starter.txt'),'utf8',(err,data) => {
-    if (err) throw err;
-    console.log(data);
-}
-)
-console.log('Hello..........')
+// fs.readFile(path.join(__dirname,'starter.txt'),'utf8',(err,data) => {
+//     if (err) throw err;
+//     console.log(data);
+// }
+// )
+// console.log('Hello..........')
 
-fs.writeFile(path.join(__dirname,'reply.txt'),'Nice to meet you',(err) => {
-    if(err) throw err;
-    console.log('Write Complete')
-})
+// fs.writeFile(path.join(__dirname,'reply.txt'),'Nice to meet you',(err) => {
+//     if(err) throw err;
+//     console.log('Write Complete')
+// })
 
-fs.readFile(path.join(__dirname,'reply.txt'),'utf8',(err,data) => {
-    if (err) throw err;
-    console.log(`The reply.txt is:   ${data}`)
-})
+// fs.readFile(path.join(__dirname,'reply.txt'),'utf8',(err,data) => {
+//     if (err) throw err;
+//     console.log(`The reply.txt is:   ${data}`)
+// })
 
-fs.appendFile(path.join(__dirname,'testing.txt'),'Testing text',(err) => {
-    if (err) throw err;
-    console.log('Append complete')
-})
+// fs.appendFile(path.join(__dirname,'testing.txt'),'Testing text',(err) => {
+//     if (err) throw err;
+//     console.log('Append complete')
+// })
 
-process.on('uncaughtException', err => {
-    console.error(`There was an uncaught error: ${err}`);
-    process.exit(1);
-})
+// process.on('uncaughtException', err => {
+//     console.error(`There was an uncaught error: ${err}`);
+//     process.exit(1);
+// })
+
