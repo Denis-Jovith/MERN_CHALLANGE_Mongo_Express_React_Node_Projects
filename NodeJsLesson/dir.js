03b08a1f03ffaf08ad68fc21ef5f7bd8c3470856
Denis Jovith <denisjovitusbuberwa@gmail.com>
2026-02-12 09:35:13 +0300

    fs.existSync learned

--- NodeJsLesson/dir.js
+++ NodeJsLesson/dir.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 
-fs.mkdir('./new',(err) => {
+if(!fs.existsSync('./new')) {
+    fs.mkdir('./new',(err) => {
     if (err) throw err;
     console.log('Directory Created')
 })
+}
+
