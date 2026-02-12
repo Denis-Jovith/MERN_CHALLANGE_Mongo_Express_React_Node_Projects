27e69eacb4f0c005d2038faf9c57635d42e37667
Denis Jovith <denisjovitusbuberwa@gmail.com>
2026-02-12 08:28:32 +0300

    reading file

--- NodeJsLesson/index.js
+++ NodeJsLesson/index.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
+const path= require('path');
 
-fs.readFile('./starter.txt','utf8',(err,data) => {
+// fs.readFile('./starter.txt','utf8',(err,data) => {
+//     if (err) throw err;
+//     console.log(data);
+// })
+
+fs.readFile(path.join(__dirname,'starter.txt'),'utf8',(err,data) => {
     if (err) throw err;
     console.log(data);
-})
-
+}
+)
 console.log('Hello..........')
 
 process.on('uncaughtException', err => {
