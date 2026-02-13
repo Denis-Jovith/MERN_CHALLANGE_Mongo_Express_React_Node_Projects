38add38879b2c677f152dec35d361cfd7f17833e
Denis Jovith <denisjovitusbuberwa@gmail.com>
2026-02-13 15:19:53 +0300

    managed to start the server and print something in local host using the req and respond and arrow functions

--- NodeJsLesson/WebServer/server.js
+++ NodeJsLesson/WebServer/server.js
@@ -5,14 +5,33 @@ const fsPromises = require('fs').promises;
 
 const logEvents = require('./logEvents');
 const EvenEmitter = require('events');
-class Emitter extends EventEmitter {};
+class Emitter extends EvenEmitter{};
 
+//initialize object
 const myEmmitter = new Emitter();
 
 const PORT = process.env.PORT || 3500;
 
 const server = http.createServer((req,res) => {
-    console.log(req.url,req.method);
+    try{
+        console.log(req.url,req.method);
+
+        let filePath;
+
+        if(req.url === '/' || req.url === '/index.html'){
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+
+            filePath = path.join(__dirname,'views','index.html')
+
+            fs.readFile(filePath,'utf8',(err,data) => {
+                if (err) throw err;
+                res.end(data);
+            })
+        }
+    } catch (error) {
+
+    }
 })
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
